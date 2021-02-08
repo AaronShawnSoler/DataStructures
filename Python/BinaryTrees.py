b7768143d8537a393c19b8d4590785530e0c6a38
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2021-02-08 00:21:11 -0500

    dfs return arrays

--- Python/BinaryTrees.py
+++ Python/BinaryTrees.py
@@ -1,53 +1,67 @@
 class Node:
-    def __init__(self, val, left, right):
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return f"{self.val}"
 
 
 class BinaryTree:
-    def __init__(self, root):
-        self.root = root if root else None
+    def __init__(self):
+        self.root = None
 
     def insert(self, node):
         if not self.root:
             self.root = node
         else:
-            while node:
-                curr = self.root
+            curr = self.root
+            while curr:
                 if node.val > curr.val:
                     if curr.right:
                         curr = curr.right
                     else:
                         curr.right = node
-                        node = None
                 elif node.val < curr.val:
                     if curr.left:
                         curr = curr.left
                     else:
                         curr.left = node
-                        node = None
+                else:
+                    return
 
-    def dfsPreOrder(self, root):
-        if not root:
-            return
+    def preOrder(self, root, arr=[]):
+        if not root.left and not root.right:
+            return [root.val]
 
-        print(root.val)
-        self.dfsPreOrder(root.left)
-        self.dfsPreOrder(root.right)
+        return [root.val] + self.preOrder(root.left) + self.preOrder(root.right)
 
-    def dfsInOrder(self, root):
-        if not root:
-            return
+    def inOrder(self, root):
+        if not root.left and not root.right:
+            return [root.val]
 
-        self.dfsPreOrder(root.left)
-        print(root.val)
-        self.dfsPreOrder(root.right)
+        return self.inOrder(root.left) + [root.val] + self.inOrder(root.right)
 
-    def dfsPostOrder(self, root):
-        if not root:
-            return
+    def postOrder(self, root, arr=[]):
+        if not root.left and not root.right:
+            return [root.val]
 
-        self.dfsPreOrder(root.left)
-        self.dfsPreOrder(root.right)
-        print(root.val)
+        return self.postOrder(root.left) + self.postOrder(root.right) + [root.val]
+
+
+BT = BinaryTree()
+
+BT.insert(Node(6))
+
+BT.insert(Node(4))
+BT.insert(Node(8))
+
+BT.insert(Node(3))
+BT.insert(Node(5))
+BT.insert(Node(7))
+BT.insert(Node(9))
+
+print(BT.preOrder(BT.root))
+print(BT.inOrder(BT.root))
+print(BT.postOrder(BT.root))
