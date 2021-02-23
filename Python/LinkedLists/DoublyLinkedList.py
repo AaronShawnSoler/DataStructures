840f0ddfe18ab92f8e70af0c43bfaf7741dee887
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2021-02-22 19:36:09 -0500

    tasks added

--- Python/LinkedLists/DoublyLinkedList.py
+++ Python/LinkedLists/DoublyLinkedList.py
@@ -10,3 +10,42 @@ class DLL:
         self.length = 0
         self.head = None
         self.tail = None
+
+    def show(self):
+        return
+
+    # adds node to the tail
+    def push(self, val):
+        return
+
+    # removes node from the tail
+    def pop(self):
+        return
+
+    # adds node to the head
+    def unshift(self, val):
+        return
+
+    # removes node from the head
+    def shift(self):
+        return
+
+    # gets node at index
+    def get(self, index):
+        return
+
+    # updates value of node at index
+    def put(self, index, val):
+        return
+
+    # inserts node at index
+    def insert(self, index, val):
+        return
+
+    # removes node at index
+    def remove(self, index):
+        return
+
+    # reverses the list
+    def reverse(self):
+        return
